4bd53a734cd55d618c4726baa754ca5408b4c785
ItsLiang62 <andersonwlx62@gmail.com>
2025-03-09 15:10:15 +0800

    added insertRow method for employeeTable class, added table base class, modified frmEmployeeSignUp code

--- Shared Class Library/employee_table.cs
+++ Shared Class Library/employee_table.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 
 namespace Shared_Class_Library
 {
-    public class employee_table
+    public class employee_table : table, ITable 
     {
+        public employee_table(SqlConnection conn) : base(conn)
+        {
+        }
 
+        public void insertRow(string employeeID, string employeeName, string position, string gender, string email, string phoneNumber, string dob, string accountPassword)
+        {
+            string query = "insert into Employee ("
+        }
     }
 }
---
+++ Shared Class Library/table.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace Shared_Class_Library
+{
+    public abstract class table
+    {
+        protected SqlConnection Conn
+        { get; set; }
+
+        public table(SqlConnection conn)
+        {
+            Conn = conn;
+        }
+    }
+}
