e702011173547022c31152ef8956f037aa6ebf55
ItsLiang62 <andersonwlx62@gmail.com>
2025-03-11 02:14:26 +0800

    created new employee_table methods for database interaction

--- Shared Class Library/employee_table.cs
+++ Shared Class Library/employee_table.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
+using Microsoft.Identity.Client;
 
 namespace Shared_Class_Library
 {
@@ -13,7 +15,7 @@ namespace Shared_Class_Library
         {
         }
 
-        public void insertRow(string employeeID, string employeeName, string position, string gender, string email, string phoneNumber, string dob, string accountPassword)
+        public void InsertRow(string employeeID, string employeeName, string position, string gender, string email, string phoneNumber, string dob, string accountPassword)
         {
 
             using (SqlConnection conn = new SqlConnection(ConnectionString))
@@ -36,6 +38,132 @@ namespace Shared_Class_Library
 
                     cmd.ExecuteNonQuery();
                 }
+            }
+        }
+
+        public object GetValue(string employeeID, string column)
+        {
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                string query = $"SELECT {column} FROM Employee WHERE EmployeeID = @employeeID";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@employeeID", employeeID);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return reader[column];
+                        }
+                        else
+                        {
+                            throw new Exception("Cannot find data. Are you sure you entered the Employee ID and column name correctly?");
+                        }
+                    }
+                }
+
+            }
+        }
+
+        public List<object> GetColumnValues(string column)
+        {
+
+            List<object> columnValues = new List<object>();
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                string query = $"SELECT {column} FROM Employee";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            columnValues.Add(reader[column]);
+                        }
+                        if (columnValues.Count > 0)
+                        {
+                            return columnValues;
+                        }
+                        else
+                        {
+                            throw new Exception("Cannot find column. Are you sure you entered the column name correctly?");
+                        }
+                    }
+                }
+
+            }
+        }
+
+        public List<object> GetRowValues(string employeeID)
+        {
+
+            List<object> rowValues = new List<object>();
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                string query = $"SELECT * FROM Employee WHERE EmployeeID = @employeeID";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@employeeID", employeeID);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            rowValues.Add(reader["EmployeeID"]);
+                            rowValues.Add(reader["EmployeeName"]);
+                            rowValues.Add(reader["Position"]);
+                            rowValues.Add(reader["Gender"]);
+                            rowValues.Add(reader["Email"]);
+                            rowValues.Add(reader["Phone"]);
+                            rowValues.Add(reader["DOB"]);
+                            rowValues.Add(reader["AccountPassword"]);
+
+                            return rowValues;
+                        }
+                        else
+                        {
+                            throw new Exception("Cannot find unique row. Are you sure you entered Employee ID correctly?");
+                        }
+                    }
+                }
+
+            }
+        }
+
+        public string GetEmployeeIDWithEmail(string email)
+        {
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                string query = $"SELECT EmployeeID FROM Employee WHERE Email = @email";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@email", email);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return reader["EmployeeID"].ToString();
+                        }
+                        else
+                        {
+                            throw new Exception("Cannot find Employee ID with the provided Email. Are you sure you entered the Email correctly?");
+                        }
+                    }
+                }
+
             }
         }
     }
