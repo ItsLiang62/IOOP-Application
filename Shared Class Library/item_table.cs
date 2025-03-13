bed11f1857cd5fa3c7b156a875090294eec34241
ItsLiang62 <andersonwlx62@gmail.com>
2025-03-14 00:30:26 +0800

    added basic methods (insert row, get data, column, row) in item_table.cs

--- Shared Class Library/item_table.cs
+++ Shared Class Library/item_table.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 
 namespace Shared_Class_Library
 {
@@ -10,6 +11,115 @@ namespace Shared_Class_Library
     {
         public ItemTable(string connectionString) : base(connectionString)
         {
+        }
+
+
+        public object GetValue(string itemNumber, string column)
+        {
+            List<string> allowedColumns = new List<string> { "ItemNumber", "ItemName", "Price", "Category" };
+
+            if (!allowedColumns.Contains(column))
+            {
+                throw new Exception("Invalid column name. Please enter a correct column.");
+            }
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                string query = $"SELECT {column} FROM Item WHERE ItemNumber = @ItemNumber";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@ItemNumber", itemNumber);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return reader[column];
+                        }
+                        else
+                        {
+                            throw new Exception("Cannot find data. Are you sure you entered the ItemNumber and column name correctly?");
+                        }
+                    }
+                }
+
+            }
+        }
+
+        public List<object> GetColumnValues(string column)
+        {
+            List<string> allowedColumns = new List<string> { "ItemNumber", "ItemName", "Price", "Category" };
+
+            if (!allowedColumns.Contains(column))
+            {
+                throw new Exception("Invalid column name. Please enter a correct column.");
+            }
+
+            List<object> columnValues = new List<object>();
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                string query = $"SELECT {column} FROM Item";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            columnValues.Add(reader[column]);
+                        }
+                        if (columnValues.Count > 0)
+                        {
+                            return columnValues;
+                        }
+                        else
+                        {
+                            throw new Exception("Cannot find column. Are you sure you entered the column name correctly?");
+                        }
+                    }
+                }
+
+            }
+        }
+
+        public List<object> GetRowValues(string itemNumber)
+        {
+
+            List<object> rowValues = new List<object>();
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                string query = $"SELECT * FROM Item WHERE ItemNumber = @ItemNumber";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@ItemNumber", itemNumber);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            rowValues.Add(reader["ItemNumber"]);
+                            rowValues.Add(reader["ItemName"]);
+                            rowValues.Add(reader["Price"]);
+                            rowValues.Add(reader["Category"]);
+
+                            return rowValues;
+                        }
+                        else
+                        {
+                            throw new Exception("Cannot find unique row. Are you sure you entered ItemName correctly?");
+                        }
+                    }
+                }
+
+            }
         }
     }
 }
