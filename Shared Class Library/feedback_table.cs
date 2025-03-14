0b3a7161a60e6dea54d554a5cd7b42c39354717e
ItsLiang62 <andersonwlx62@gmail.com>
2025-03-14 12:48:33 +0800

    completed basic methods (insertRow, getValue, getColumnValues, getRowValues) for all tables' .cs file

--- Shared Class Library/feedback_table.cs
+++ Shared Class Library/feedback_table.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 
 namespace Shared_Class_Library
 {
@@ -10,6 +11,136 @@ namespace Shared_Class_Library
     {
         public FeedbackTable(string connectionString) : base(connectionString)
         {
+        }
+
+        public void InsertRow(string feedbackID, string customerID, string feedbackSentence, int rating)
+        {
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+
+                string query = "INSERT INTO Feedback (FeedbackID, CustomerID, FeedbackSentence, Rating)" +
+                               "VALUES (@FeedbackID, @CustomerID, @FeedbackSentence, @Rating)";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@FeedbackID", feedbackID);
+                    cmd.Parameters.AddWithValue("@CustomerID", customerID);
+                    cmd.Parameters.AddWithValue("@FeedbackSentence", feedbackSentence);
+                    cmd.Parameters.AddWithValue("@Rating", rating);
+
+                    cmd.ExecuteNonQuery();
+
+                }
+            }
+        }
+        public object GetValue(string feedbackID, string column)
+        {
+            List<string> allowedColumns = new List<string> { "FeedbackID, CustomerID, FeedbackSentence, Rating" };
+
+            if (!allowedColumns.Contains(column))
+            {
+                throw new Exception("Invalid column name. Please enter a correct column.");
+            }
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                string query = $"SELECT {column} FROM Feedback WHERE FeedbackID = @FeedbackID";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@FeedbackID", feedbackID);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return reader[column];
+                        }
+                        else
+                        {
+                            throw new Exception("Cannot find data. Are you sure you entered the FeedbackID correctly?");
+                        }
+                    }
+                }
+
+            }
+        }
+
+        public List<object> GetColumnValues(string column)
+        {
+            List<string> allowedColumns = new List<string> { "FeedbackID, CustomerID, FeedbackSentence, Rating" };
+
+            if (!allowedColumns.Contains(column))
+            {
+                throw new Exception("Invalid column name. Please enter a correct column.");
+            }
+
+            List<object> columnValues = new List<object>();
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                string query = $"SELECT {column} FROM Feedback";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            columnValues.Add(reader[column]);
+                        }
+                        if (columnValues.Count > 0)
+                        {
+                            return columnValues;
+                        }
+                        else
+                        {
+                            throw new Exception("Cannot find column. Are you sure you entered the column name correctly?");
+                        }
+                    }
+                }
+
+            }
+        }
+
+        public List<object> GetRowValues(string feedbackID)
+        {
+
+            List<object> rowValues = new List<object>();
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                string query = $"SELECT * FROM Feedback WHERE FeedbackID = @FeedbackID";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@FeedbackID", feedbackID);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            rowValues.Add(reader["FeedbackID"]);
+                            rowValues.Add(reader["CustomerID"]);
+                            rowValues.Add(reader["FeedbackSentence"]);
+                            rowValues.Add(reader["Rating"]);
+
+                            return rowValues;
+                        }
+                        else
+                        {
+                            throw new Exception("Cannot find unique row. Are you sure you entered FeedbackID correctly?");
+                        }
+                    }
+                }
+
+            }
         }
     }
 }
--- Shared Class Library/hall_reservation_table.cs
+++ Shared Class Library/hall_reservation_table.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 
 namespace Shared_Class_Library
 {
@@ -10,6 +11,140 @@ namespace Shared_Class_Library
     {
         public HallReservationTable(string connectionString) : base(connectionString)
         {
+        }
+
+        public void InsertRow(string reservationID, string hallNumber, string customerID, string eventType, string reservationStatus, string remarks)
+        {
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+
+                string query = "INSERT INTO HallReservation (ReservationID, HallNumber, CustomerID, EventType, ReservationStatus, Remarks) " +
+                               "VALUES (@ReservationID, @HallNumber, @CustomerID, @EventType, @ReservatioinStatus, @Remarks)";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@ReservationID", reservationID);
+                    cmd.Parameters.AddWithValue("@HallNumber", hallNumber);
+                    cmd.Parameters.AddWithValue("@CustomerID", customerID);
+                    cmd.Parameters.AddWithValue("@EventType", eventType);
+                    cmd.Parameters.AddWithValue("@ReservationStatus", reservationStatus);
+                    cmd.Parameters.AddWithValue("@Remarks", remarks);
+
+                    cmd.ExecuteNonQuery();
+
+                }
+            }
+        }
+        public object GetValue(string reservationID, string column)
+        {
+            List<string> allowedColumns = new List<string> { "ReservatonID, HallNumber, CustomerID, EventType, ReservationStatus, Remarks" };
+
+            if (!allowedColumns.Contains(column))
+            {
+                throw new Exception("Invalid column name. Please enter a correct column.");
+            }
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                string query = $"SELECT {column} FROM HallReservation WHERE ReservationID = @ReservationID";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@ReservationID", reservationID);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return reader[column];
+                        }
+                        else
+                        {
+                            throw new Exception("Cannot find data. Are you sure you entered the ReservationID correctly?");
+                        }
+                    }
+                }
+
+            }
+        }
+
+        public List<object> GetColumnValues(string column)
+        {
+            List<string> allowedColumns = new List<string> { "ReservatonID, HallNumber, CustomerID, EventType, ReservationStatus, Remarks" };
+
+            if (!allowedColumns.Contains(column))
+            {
+                throw new Exception("Invalid column name. Please enter a correct column.");
+            }
+
+            List<object> columnValues = new List<object>();
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                string query = $"SELECT {column} FROM HallReservation";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            columnValues.Add(reader[column]);
+                        }
+                        if (columnValues.Count > 0)
+                        {
+                            return columnValues;
+                        }
+                        else
+                        {
+                            throw new Exception("Cannot find column. Are you sure you entered the column name correctly?");
+                        }
+                    }
+                }
+
+            }
+        }
+
+        public List<object> GetRowValues(string reservationID)
+        {
+
+            List<object> rowValues = new List<object>();
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                string query = $"SELECT * FROM HallReservation WHERE ReservationID = @ReservationID";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@ReservationID", reservationID);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            rowValues.Add(reader["ReservationID"]);
+                            rowValues.Add(reader["HallNumber"]);
+                            rowValues.Add(reader["CustomerID"]);
+                            rowValues.Add(reader["EventType"]);
+                            rowValues.Add(reader["ReservationStatus"]);
+                            rowValues.Add(reader["Remarks"]);
+
+                            return rowValues;
+                        }
+                        else
+                        {
+                            throw new Exception("Cannot find unique row. Are you sure you entered ReservationID correctly?");
+                        }
+                    }
+                }
+
+            }
         }
     }
 }
