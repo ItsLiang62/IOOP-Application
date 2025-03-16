56e69e46fd92ea2727cf131482e411965b6f65bb
badr34555555556 <157200520+badr34555555556@users.noreply.github.com>
2025-03-16 15:17:37 +0800

    added a new form orderfood

---
+++ Foodle Point Management System/OrderFoodForm.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Foodle_Point_Management_System
+{
+    partial class OrderFoodForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgvMenuItems = new System.Windows.Forms.DataGridView();
+            this.dgvCart = new System.Windows.Forms.DataGridView();
+            this.btnAddToCart = new System.Windows.Forms.Button();
+            this.btnEditCart = new System.Windows.Forms.Button();
+            this.btnRemoveCart = new System.Windows.Forms.Button();
+            this.btnProceedToPayment = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMenuItems)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(333, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Food menu";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(378, 77);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(100, 22);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(270, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "search:";
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Location = new System.Drawing.Point(397, 131);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(121, 24);
+            this.cmbCategory.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(234, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "gategories:";
+            // 
+            // dgvMenuItems
+            // 
+            this.dgvMenuItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMenuItems.Location = new System.Drawing.Point(42, 197);
+            this.dgvMenuItems.Name = "dgvMenuItems";
+            this.dgvMenuItems.RowHeadersWidth = 51;
+            this.dgvMenuItems.RowTemplate.Height = 24;
+            this.dgvMenuItems.Size = new System.Drawing.Size(240, 150);
+            this.dgvMenuItems.TabIndex = 5;
+            // 
+            // dgvCart
+            // 
+            this.dgvCart.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCart.Location = new System.Drawing.Point(313, 197);
+            this.dgvCart.Name = "dgvCart";
+            this.dgvCart.RowHeadersWidth = 51;
+            this.dgvCart.RowTemplate.Height = 24;
+            this.dgvCart.Size = new System.Drawing.Size(240, 150);
+            this.dgvCart.TabIndex = 6;
+            // 
+            // btnAddToCart
+            // 
+            this.btnAddToCart.Location = new System.Drawing.Point(121, 372);
+            this.btnAddToCart.Name = "btnAddToCart";
+            this.btnAddToCart.Size = new System.Drawing.Size(75, 23);
+            this.btnAddToCart.TabIndex = 7;
+            this.btnAddToCart.Text = "Add";
+            this.btnAddToCart.UseVisualStyleBackColor = true;
+            // 
+            // btnEditCart
+            // 
+            this.btnEditCart.Location = new System.Drawing.Point(338, 385);
+            this.btnEditCart.Name = "btnEditCart";
+            this.btnEditCart.Size = new System.Drawing.Size(75, 23);
+            this.btnEditCart.TabIndex = 8;
+            this.btnEditCart.Text = "Edit";
+            this.btnEditCart.UseVisualStyleBackColor = true;
+            // 
+            // btnRemoveCart
+            // 
+            this.btnRemoveCart.Location = new System.Drawing.Point(455, 385);
+            this.btnRemoveCart.Name = "btnRemoveCart";
+            this.btnRemoveCart.Size = new System.Drawing.Size(75, 23);
+            this.btnRemoveCart.TabIndex = 9;
+            this.btnRemoveCart.Text = "remove";
+            this.btnRemoveCart.UseVisualStyleBackColor = true;
+            // 
+            // btnProceedToPayment
+            // 
+            this.btnProceedToPayment.Location = new System.Drawing.Point(251, 415);
+            this.btnProceedToPayment.Name = "btnProceedToPayment";
+            this.btnProceedToPayment.Size = new System.Drawing.Size(75, 23);
+            this.btnProceedToPayment.TabIndex = 10;
+            this.btnProceedToPayment.Text = "Pay";
+            this.btnProceedToPayment.UseVisualStyleBackColor = true;
+            // 
+            // OrderFoodForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnProceedToPayment);
+            this.Controls.Add(this.btnRemoveCart);
+            this.Controls.Add(this.btnEditCart);
+            this.Controls.Add(this.btnAddToCart);
+            this.Controls.Add(this.dgvCart);
+            this.Controls.Add(this.dgvMenuItems);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbCategory);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label1);
+            this.Name = "OrderFoodForm";
+            this.Text = "OrderFoodForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMenuItems)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgvMenuItems;
+        private System.Windows.Forms.DataGridView dgvCart;
+        private System.Windows.Forms.Button btnAddToCart;
+        private System.Windows.Forms.Button btnEditCart;
+        private System.Windows.Forms.Button btnRemoveCart;
+        private System.Windows.Forms.Button btnProceedToPayment;
+    }
+}
