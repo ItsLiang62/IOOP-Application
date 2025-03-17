09899cd6dba3c720f9582d2bcf2a0624cafc32bb
Mo3203 <Mo3203hammad@gmail.com>
2025-03-18 07:18:26 +0800

    admin panel
    admin part

---
+++ Foodle Point Management System/AddUser.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Foodle_Point_Management_System
+{
+    partial class AddUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblRole = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.cmbRole = new System.Windows.Forms.ComboBox();
+            this.btnSaveChanges = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(335, 206);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(116, 25);
+            this.lblUsername.TabIndex = 0;
+            this.lblUsername.Text = "Username:";
+            // 
+            // lblRole
+            // 
+            this.lblRole.AutoSize = true;
+            this.lblRole.Location = new System.Drawing.Point(335, 281);
+            this.lblRole.Name = "lblRole";
+            this.lblRole.Size = new System.Drawing.Size(62, 25);
+            this.lblRole.TabIndex = 1;
+            this.lblRole.Text = "Role:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(335, 358);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(71, 25);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "Email:";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(335, 430);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(80, 25);
+            this.lblPhone.TabIndex = 3;
+            this.lblPhone.Text = "Phone:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(335, 496);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(112, 25);
+            this.lblPassword.TabIndex = 4;
+            this.lblPassword.Text = "Password:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(641, 199);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(196, 31);
+            this.txtUsername.TabIndex = 5;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(641, 358);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(196, 31);
+            this.txtEmail.TabIndex = 6;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(641, 424);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(196, 31);
+            this.txtPhone.TabIndex = 7;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(641, 490);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(196, 31);
+            this.txtPassword.TabIndex = 8;
+            // 
+            // cmbRole
+            // 
+            this.cmbRole.FormattingEnabled = true;
+            this.cmbRole.Location = new System.Drawing.Point(641, 281);
+            this.cmbRole.Name = "cmbRole";
+            this.cmbRole.Size = new System.Drawing.Size(196, 33);
+            this.cmbRole.TabIndex = 9;
+            // 
+            // btnSaveChanges
+            // 
+            this.btnSaveChanges.Location = new System.Drawing.Point(729, 611);
+            this.btnSaveChanges.Name = "btnSaveChanges";
+            this.btnSaveChanges.Size = new System.Drawing.Size(197, 56);
+            this.btnSaveChanges.TabIndex = 10;
+            this.btnSaveChanges.Text = "Save Changes";
+            this.btnSaveChanges.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 25);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Add User";
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(340, 611);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(197, 56);
+            this.btnBack.TabIndex = 12;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // AddUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1368, 964);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSaveChanges);
+            this.Controls.Add(this.cmbRole);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblRole);
+            this.Controls.Add(this.lblUsername);
+            this.Name = "AddUser";
+            this.Text = "AddUser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblRole;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.ComboBox cmbRole;
+        private System.Windows.Forms.Button btnSaveChanges;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBack;
+    }
+}
---
+++ Foodle Point Management System/AdminHomePage.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Foodle_Point_Management_System
+{
+    partial class AdminHomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnManageUser = new System.Windows.Forms.Button();
+            this.btnViewFeedback = new System.Windows.Forms.Button();
+            this.btnViewSalesReport = new System.Windows.Forms.Button();
+            this.btnUpdateProfile = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnManageUser
+            // 
+            this.btnManageUser.Location = new System.Drawing.Point(304, 225);
+            this.btnManageUser.Name = "btnManageUser";
+            this.btnManageUser.Size = new System.Drawing.Size(244, 75);
+            this.btnManageUser.TabIndex = 0;
+            this.btnManageUser.Text = "Manage User";
+            this.btnManageUser.UseVisualStyleBackColor = true;
+            // 
+            // btnViewFeedback
+            // 
+            this.btnViewFeedback.Location = new System.Drawing.Point(301, 321);
+            this.btnViewFeedback.Name = "btnViewFeedback";
+            this.btnViewFeedback.Size = new System.Drawing.Size(247, 75);
+            this.btnViewFeedback.TabIndex = 1;
+            this.btnViewFeedback.Text = "View Feedback";
+            this.btnViewFeedback.UseVisualStyleBackColor = true;
+            // 
+            // btnViewSalesReport
+            // 
+            this.btnViewSalesReport.Location = new System.Drawing.Point(584, 226);
+            this.btnViewSalesReport.Name = "btnViewSalesReport";
+            this.btnViewSalesReport.Size = new System.Drawing.Size(244, 74);
+            this.btnViewSalesReport.TabIndex = 2;
+            this.btnViewSalesReport.Text = "View Sales Report";
+            this.btnViewSalesReport.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdateProfile
+            // 
+            this.btnUpdateProfile.Location = new System.Drawing.Point(584, 321);
+            this.btnUpdateProfile.Name = "btnUpdateProfile";
+            this.btnUpdateProfile.Size = new System.Drawing.Size(247, 74);
+            this.btnUpdateProfile.TabIndex = 3;
+            this.btnUpdateProfile.Text = "Update Profile";
+            this.btnUpdateProfile.UseVisualStyleBackColor = true;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Location = new System.Drawing.Point(499, 453);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(139, 49);
+            this.btnLogout.TabIndex = 4;
+            this.btnLogout.Text = "LogOut";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            // 
+            // AdminHomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1147, 740);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.btnUpdateProfile);
+            this.Controls.Add(this.btnViewSalesReport);
+            this.Controls.Add(this.btnViewFeedback);
+            this.Controls.Add(this.btnManageUser);
+            this.Name = "AdminHomePage";
+            this.Text = "AdminHomePage";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnManageUser;
+        private System.Windows.Forms.Button btnViewFeedback;
+        private System.Windows.Forms.Button btnViewSalesReport;
+        private System.Windows.Forms.Button btnUpdateProfile;
+        private System.Windows.Forms.Button btnLogout;
+    }
+}
