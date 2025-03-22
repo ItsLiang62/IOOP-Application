969d987b25a417eceb672ebf5cdefd183faff9da
ItsLiang62 <andersonwlx62@gmail.com>
2025-03-23 02:32:23 +0800

    Update customer_login.cs

--- Foodle Point Management System/customer_login.cs
+++ Foodle Point Management System/customer_login.cs
@@ -13,7 +13,9 @@ namespace Foodle_Point_Management_System
 {
     public partial class frmCustomerLogin: Form
     {
+        //
         private CustomerTable myCustomerTable = new CustomerTable();
+
         private string CustomerID
         { get; set; }
 
