06edc53b555e065cbb76b924c0a073ff6962268c
AdrianLiewRenQian <adrian33888586@gmail.com>
2025-03-24 15:18:08 +0800

    push

--- Foodle Point Management System/OrderFoodForm.Designer.cs
+++ Foodle Point Management System/OrderFoodForm.Designer.cs
@@ -47,7 +47,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(333, 9);
+            this.label1.Location = new System.Drawing.Point(333, 8);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(145, 29);
             this.label1.TabIndex = 0;
@@ -55,9 +55,9 @@
             // 
             // txtSearch
             // 
-            this.txtSearch.Location = new System.Drawing.Point(378, 77);
+            this.txtSearch.Location = new System.Drawing.Point(378, 72);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(100, 22);
+            this.txtSearch.Size = new System.Drawing.Size(100, 25);
             this.txtSearch.TabIndex = 1;
             this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
             // 
@@ -65,7 +65,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(270, 70);
+            this.label2.Location = new System.Drawing.Point(270, 66);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(98, 29);
             this.label2.TabIndex = 2;
@@ -74,9 +74,9 @@
             // cmbCategory
             // 
             this.cmbCategory.FormattingEnabled = true;
-            this.cmbCategory.Location = new System.Drawing.Point(397, 131);
+            this.cmbCategory.Location = new System.Drawing.Point(397, 123);
             this.cmbCategory.Name = "cmbCategory";
-            this.cmbCategory.Size = new System.Drawing.Size(121, 24);
+            this.cmbCategory.Size = new System.Drawing.Size(121, 23);
             this.cmbCategory.TabIndex = 3;
             this.cmbCategory.SelectedIndexChanged += new System.EventHandler(this.cmbCategory_SelectedIndexChanged);
             // 
@@ -84,7 +84,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(234, 126);
+            this.label3.Location = new System.Drawing.Point(234, 118);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(145, 29);
             this.label3.TabIndex = 4;
@@ -93,28 +93,28 @@
             // dgvMenuItems
             // 
             this.dgvMenuItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvMenuItems.Location = new System.Drawing.Point(4, 197);
+            this.dgvMenuItems.Location = new System.Drawing.Point(0, 185);
             this.dgvMenuItems.Name = "dgvMenuItems";
             this.dgvMenuItems.RowHeadersWidth = 51;
             this.dgvMenuItems.RowTemplate.Height = 24;
-            this.dgvMenuItems.Size = new System.Drawing.Size(547, 184);
+            this.dgvMenuItems.Size = new System.Drawing.Size(547, 172);
             this.dgvMenuItems.TabIndex = 5;
             // 
             // dgvCart
             // 
             this.dgvCart.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvCart.Location = new System.Drawing.Point(809, 170);
+            this.dgvCart.Location = new System.Drawing.Point(809, 159);
             this.dgvCart.Name = "dgvCart";
             this.dgvCart.RowHeadersWidth = 51;
             this.dgvCart.RowTemplate.Height = 24;
-            this.dgvCart.Size = new System.Drawing.Size(541, 211);
+            this.dgvCart.Size = new System.Drawing.Size(541, 198);
             this.dgvCart.TabIndex = 6;
             // 
             // btnAddToCart
             // 
-            this.btnAddToCart.Location = new System.Drawing.Point(153, 400);
+            this.btnAddToCart.Location = new System.Drawing.Point(153, 375);
             this.btnAddToCart.Name = "btnAddToCart";
-            this.btnAddToCart.Size = new System.Drawing.Size(75, 23);
+            this.btnAddToCart.Size = new System.Drawing.Size(75, 22);
             this.btnAddToCart.TabIndex = 7;
             this.btnAddToCart.Text = "Add";
             this.btnAddToCart.UseVisualStyleBackColor = true;
@@ -122,9 +122,9 @@
             // 
             // btnEditCart
             // 
-            this.btnEditCart.Location = new System.Drawing.Point(1026, 426);
+            this.btnEditCart.Location = new System.Drawing.Point(1026, 399);
             this.btnEditCart.Name = "btnEditCart";
-            this.btnEditCart.Size = new System.Drawing.Size(75, 23);
+            this.btnEditCart.Size = new System.Drawing.Size(75, 22);
             this.btnEditCart.TabIndex = 8;
             this.btnEditCart.Text = "Edit";
             this.btnEditCart.UseVisualStyleBackColor = true;
@@ -132,9 +132,9 @@
             // 
             // btnRemoveCart
             // 
-            this.btnRemoveCart.Location = new System.Drawing.Point(1227, 426);
+            this.btnRemoveCart.Location = new System.Drawing.Point(1227, 399);
             this.btnRemoveCart.Name = "btnRemoveCart";
-            this.btnRemoveCart.Size = new System.Drawing.Size(75, 23);
+            this.btnRemoveCart.Size = new System.Drawing.Size(75, 22);
             this.btnRemoveCart.TabIndex = 9;
             this.btnRemoveCart.Text = "remove";
             this.btnRemoveCart.UseVisualStyleBackColor = true;
@@ -142,9 +142,9 @@
             // 
             // btnProceedToPayment
             // 
-            this.btnProceedToPayment.Location = new System.Drawing.Point(649, 563);
+            this.btnProceedToPayment.Location = new System.Drawing.Point(649, 528);
             this.btnProceedToPayment.Name = "btnProceedToPayment";
-            this.btnProceedToPayment.Size = new System.Drawing.Size(75, 23);
+            this.btnProceedToPayment.Size = new System.Drawing.Size(75, 22);
             this.btnProceedToPayment.TabIndex = 10;
             this.btnProceedToPayment.Text = "Pay";
             this.btnProceedToPayment.UseVisualStyleBackColor = true;
@@ -152,9 +152,9 @@
             // 
             // OrderFoodForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1477, 649);
+            this.ClientSize = new System.Drawing.Size(1477, 608);
             this.Controls.Add(this.btnProceedToPayment);
             this.Controls.Add(this.btnRemoveCart);
             this.Controls.Add(this.btnEditCart);
