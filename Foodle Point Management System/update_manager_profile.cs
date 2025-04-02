e02050930fb2b92bd2cef9a154de91e94667434c
ItsLiang62 <andersonwlx62@gmail.com>
2025-04-02 11:45:26 +0800

    changes

--- Foodle Point Management System/update_manager_profile.cs
+++ Foodle Point Management System/update_manager_profile.cs
@@ -11,125 +11,125 @@ using Shared_Class_Library;
 
 namespace Foodle_Point_Management_System
 {
-    public partial class frmUpdateManagerProfile: Form
-    {
-        private EmployeeTable myEmployeeTable = new EmployeeTable();
-
-        private InputChecker myChecker = new InputChecker();
-
-        private Manager ManagerUser
-        { get; set; }
-
-        private string FullName
-        { get; set; }
-
-        private string DOB
-        { get; set; }
-
-        private string Email
-        { get; set; }
-
-        private string PhoneNum
-        { get; set; }
-
-        private string Gender
-        { get; set; }
-
-        private string Password
-        { get; set; }
-
-        public frmUpdateManagerProfile(Manager managerUser)
+        public partial class frmUpdateManagerProfile: Form
         {
-            InitializeComponent();
-            ManagerUser = managerUser;
-            this.FillInputBox();
-        }
+            private EmployeeTable myEmployeeTable = new EmployeeTable();
 
-        private void btnReturnMain_Click(object sender, EventArgs e)
-        {
-            frmManagerMain managerMainPage = new frmManagerMain(ManagerUser);
-            managerMainPage.Show();
-            this.Hide();
-        }
+            private InputChecker myChecker = new InputChecker();
 
-        private void FillInputBox()
-        {
-            lblShowEmployeeID.Text = ManagerUser.GetEmployeeID();
-            txtName.Text = ManagerUser.GetFullName();
-            lblShowPosition.Text = ManagerUser.GetPosition();
-            cmbGender.Text = ManagerUser.GetGender();
-            txtDOB.Text = ManagerUser.GetDOB();
-            txtPhoneNum.Text = ManagerUser.GetPhoneNum();
-            txtEmail.Text = ManagerUser.GetEmail();
-            txtPassword.Text = ManagerUser.GetPassword();
-        }
+            private Manager ManagerUser
+            { get; set; }
 
-        private void btnSave_Click(object sender, EventArgs e)
-        {
-            string employeeID = lblShowEmployeeID.Text;
+            private string FullName
+            { get; set; }
 
-            if (AllInputValid(out string messageBoxErrorMessage))
+            private string DOB
+            { get; set; }
+
+            private string Email
+            { get; set; }
+
+            private string PhoneNum
+            { get; set; }
+
+            private string Gender
+            { get; set; }
+
+            private string Password
+            { get; set; }
+
+            public frmUpdateManagerProfile(Manager managerUser)
             {
-                FullName = txtName.Text;
-                Email = txtEmail.Text;
-                PhoneNum = txtPhoneNum.Text;
-                DOB = txtDOB.Text;
-                Gender = cmbGender.Text;
-                Password = txtPassword.Text;
-
-                try
-                {
-                    myEmployeeTable.UpdateValue(employeeID, "EmployeeName", FullName);
-                    myEmployeeTable.UpdateValue(employeeID, "Gender", Gender);
-                    myEmployeeTable.UpdateValue(employeeID, "DOB", DOB);
-                    myEmployeeTable.UpdateValue(employeeID, "Email", Email);
-                    myEmployeeTable.UpdateValue(employeeID, "PhoneNumber", PhoneNum);
-                    myEmployeeTable.UpdateValue(employeeID, "Password", Password);
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                    return;
-                }
-                MessageBox.Show("Successfully updated profile.");
-                ManagerUser.Refresh();
+                InitializeComponent();
+                ManagerUser = managerUser;
+                this.FillInputBox();
             }
-            else
+
+            private void btnReturnMain_Click(object sender, EventArgs e)
             {
-                MessageBox.Show(messageBoxErrorMessage);
+                frmManagerMain managerMainPage = new frmManagerMain(ManagerUser);
+                managerMainPage.Show();
+                this.Hide();
             }
-        }
 
-        private bool AllInputValid(out string messageBoxErrorMessage)
-        {
-            bool validName = myChecker.IsTextOnly(txtName.Text, out string eName, "Full Name");
-            bool validDate = myChecker.IsValidDate(txtDOB.Text, out string eDOB);
-            bool validEmail = myChecker.IsValidEmail(txtEmail.Text, out string eEmail);
-            bool validPhoneNum = myChecker.IsValidPhoneNumber(txtPhoneNum.Text, out string ePhoneNum);
-
-            messageBoxErrorMessage = String.Empty;
-
-            foreach (string error in new string[] { eName, eDOB, eEmail, ePhoneNum })
+            private void FillInputBox()
             {
-                if (error != "No error")
+                lblShowEmployeeID.Text = ManagerUser.GetEmployeeID();
+                txtName.Text = ManagerUser.GetFullName();
+                lblShowPosition.Text = ManagerUser.GetPosition();
+                cmbGender.Text = ManagerUser.GetGender();
+                txtDOB.Text = ManagerUser.GetDOB();
+                txtPhoneNum.Text = ManagerUser.GetPhoneNum();
+                txtEmail.Text = ManagerUser.GetEmail();
+                txtPassword.Text = ManagerUser.GetPassword();
+            }
+
+            private void btnSave_Click(object sender, EventArgs e)
+            {
+                string employeeID = lblShowEmployeeID.Text;
+
+                if (AllInputValid(out string messageBoxErrorMessage))
                 {
-                    messageBoxErrorMessage += error + "\n";
+                    FullName = txtName.Text;
+                    Email = txtEmail.Text;
+                    PhoneNum = txtPhoneNum.Text;
+                    DOB = txtDOB.Text;
+                    Gender = cmbGender.Text;
+                    Password = txtPassword.Text;
+
+                    try
+                    {
+                        myEmployeeTable.UpdateValue(employeeID, "EmployeeName", FullName);
+                        myEmployeeTable.UpdateValue(employeeID, "Gender", Gender);
+                        myEmployeeTable.UpdateValue(employeeID, "DOB", DOB);
+                        myEmployeeTable.UpdateValue(employeeID, "Email", Email);
+                        myEmployeeTable.UpdateValue(employeeID, "PhoneNumber", PhoneNum);
+                        myEmployeeTable.UpdateValue(employeeID, "Password", Password);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        return;
+                    }
+                    MessageBox.Show("Successfully updated profile.");
+                    ManagerUser.Refresh();
+                }
+                else
+                {
+                    MessageBox.Show(messageBoxErrorMessage);
                 }
             }
 
-            if (messageBoxErrorMessage != String.Empty)
+            private bool AllInputValid(out string messageBoxErrorMessage)
             {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-        }
+                bool validName = myChecker.IsTextOnly(txtName.Text, out string eName, "Full Name");
+                bool validDate = myChecker.IsValidDate(txtDOB.Text, out string eDOB);
+                bool validEmail = myChecker.IsValidEmail(txtEmail.Text, out string eEmail);
+                bool validPhoneNum = myChecker.IsValidPhoneNumber(txtPhoneNum.Text, out string ePhoneNum);
 
-        private void frmUpdateManagerProfile_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            Application.Exit();
+                messageBoxErrorMessage = String.Empty;
+
+                foreach (string error in new string[] { eName, eDOB, eEmail, ePhoneNum })
+                {
+                    if (error != "No error")
+                    {
+                        messageBoxErrorMessage += error + "\n";
+                    }
+                }
+
+                if (messageBoxErrorMessage != String.Empty)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            private void frmUpdateManagerProfile_FormClosing(object sender, FormClosingEventArgs e)
+            {
+                Application.Exit();
+            }
         }
-    }
 }
