ed9fc5e177d6987f811fd580680671609791dba6
Mo3203 <Mo3203hammad@gmail.com>
2025-04-03 05:49:01 +0800

    bit changes

--- Foodle Point Management System/EditUser.Designer.cs
+++ Foodle Point Management System/EditUser.Designer.cs
@@ -44,66 +44,59 @@
             // lblUsernameE
             // 
             this.lblUsernameE.AutoSize = true;
-            this.lblUsernameE.Location = new System.Drawing.Point(198, 96);
-            this.lblUsernameE.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblUsernameE.Location = new System.Drawing.Point(395, 160);
             this.lblUsernameE.Name = "lblUsernameE";
-            this.lblUsernameE.Size = new System.Drawing.Size(112, 16);
+            this.lblUsernameE.Size = new System.Drawing.Size(175, 25);
             this.lblUsernameE.TabIndex = 0;
             this.lblUsernameE.Text = "Employee Name:";
             // 
             // lblRoleE
             // 
             this.lblRoleE.AutoSize = true;
-            this.lblRoleE.Location = new System.Drawing.Point(198, 143);
-            this.lblRoleE.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblRoleE.Location = new System.Drawing.Point(395, 233);
             this.lblRoleE.Name = "lblRoleE";
-            this.lblRoleE.Size = new System.Drawing.Size(58, 16);
+            this.lblRoleE.Size = new System.Drawing.Size(95, 25);
             this.lblRoleE.TabIndex = 1;
             this.lblRoleE.Text = "Position:";
             // 
             // lblEmailE
             // 
             this.lblEmailE.AutoSize = true;
-            this.lblEmailE.Location = new System.Drawing.Point(198, 198);
-            this.lblEmailE.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblEmailE.Location = new System.Drawing.Point(395, 319);
             this.lblEmailE.Name = "lblEmailE";
-            this.lblEmailE.Size = new System.Drawing.Size(44, 16);
+            this.lblEmailE.Size = new System.Drawing.Size(71, 25);
             this.lblEmailE.TabIndex = 2;
             this.lblEmailE.Text = "Email:";
             // 
             // lblPhoneNumberE
             // 
             this.lblPhoneNumberE.AutoSize = true;
-            this.lblPhoneNumberE.Location = new System.Drawing.Point(198, 246);
-            this.lblPhoneNumberE.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPhoneNumberE.Location = new System.Drawing.Point(395, 394);
             this.lblPhoneNumberE.Name = "lblPhoneNumberE";
-            this.lblPhoneNumberE.Size = new System.Drawing.Size(100, 16);
+            this.lblPhoneNumberE.Size = new System.Drawing.Size(161, 25);
             this.lblPhoneNumberE.TabIndex = 3;
             this.lblPhoneNumberE.Text = "Phone Number:";
             // 
             // txtUsernameE
             // 
-            this.txtUsernameE.Location = new System.Drawing.Point(419, 96);
-            this.txtUsernameE.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtUsernameE.Location = new System.Drawing.Point(726, 160);
             this.txtUsernameE.Name = "txtUsernameE";
-            this.txtUsernameE.Size = new System.Drawing.Size(111, 22);
+            this.txtUsernameE.Size = new System.Drawing.Size(164, 31);
             this.txtUsernameE.TabIndex = 4;
             this.txtUsernameE.TextChanged += new System.EventHandler(this.txtUsernameE_TextChanged);
             // 
             // txtEmailE
             // 
-            this.txtEmailE.Location = new System.Drawing.Point(419, 198);
-            this.txtEmailE.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtEmailE.Location = new System.Drawing.Point(726, 319);
             this.txtEmailE.Name = "txtEmailE";
-            this.txtEmailE.Size = new System.Drawing.Size(111, 22);
+            this.txtEmailE.Size = new System.Drawing.Size(164, 31);
             this.txtEmailE.TabIndex = 5;
             // 
             // txtPhoneNumberE
             // 
-            this.txtPhoneNumberE.Location = new System.Drawing.Point(419, 246);
-            this.txtPhoneNumberE.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPhoneNumberE.Location = new System.Drawing.Point(726, 394);
             this.txtPhoneNumberE.Name = "txtPhoneNumberE";
-            this.txtPhoneNumberE.Size = new System.Drawing.Size(111, 22);
+            this.txtPhoneNumberE.Size = new System.Drawing.Size(164, 31);
             this.txtPhoneNumberE.TabIndex = 6;
             // 
             // cmbRoleE
@@ -115,29 +108,26 @@
             "Chef",
             "Waiter",
             "ReservationCoordinator"});
-            this.cmbRoleE.Location = new System.Drawing.Point(419, 143);
-            this.cmbRoleE.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cmbRoleE.Location = new System.Drawing.Point(726, 233);
             this.cmbRoleE.Name = "cmbRoleE";
-            this.cmbRoleE.Size = new System.Drawing.Size(111, 24);
+            this.cmbRoleE.Size = new System.Drawing.Size(164, 33);
             this.cmbRoleE.TabIndex = 7;
             this.cmbRoleE.SelectedIndexChanged += new System.EventHandler(this.cmbRoleE_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(69, 55);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(202, 96);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 16);
+            this.label1.Size = new System.Drawing.Size(116, 25);
             this.label1.TabIndex = 8;
             this.label1.Text = "Edit Profile";
             // 
             // btnSaveChanges
             // 
-            this.btnSaveChanges.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.btnSaveChanges.Location = new System.Drawing.Point(628, 567);
+            this.btnSaveChanges.Location = new System.Drawing.Point(796, 530);
             this.btnSaveChanges.Name = "btnSaveChanges";
-            this.btnSaveChanges.Size = new System.Drawing.Size(122, 35);
+            this.btnSaveChanges.Size = new System.Drawing.Size(183, 55);
             this.btnSaveChanges.TabIndex = 9;
             this.btnSaveChanges.Text = "Save Changes";
             this.btnSaveChanges.UseVisualStyleBackColor = true;
@@ -145,10 +135,9 @@
             // 
             // btnBack
             // 
-            this.btnBack.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.btnBack.Location = new System.Drawing.Point(302, 567);
+            this.btnBack.Location = new System.Drawing.Point(307, 530);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(122, 35);
+            this.btnBack.Size = new System.Drawing.Size(183, 55);
             this.btnBack.TabIndex = 10;
             this.btnBack.Text = "Back";
             this.btnBack.UseVisualStyleBackColor = true;
@@ -156,9 +145,9 @@
             // 
             // EditUser
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(859, 529);
+            this.ClientSize = new System.Drawing.Size(1288, 762);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.btnSaveChanges);
             this.Controls.Add(this.label1);
@@ -170,7 +159,6 @@
             this.Controls.Add(this.lblEmailE);
             this.Controls.Add(this.lblRoleE);
             this.Controls.Add(this.lblUsernameE);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "EditUser";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.EditUser_FormClosing);
             this.Load += new System.EventHandler(this.EditUser_Load);
