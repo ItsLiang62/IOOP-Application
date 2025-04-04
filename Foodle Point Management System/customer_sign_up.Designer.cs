99098e5306e19a7731a216e3aeb56e78a060d8d1
ItsLiang62 <andersonwlx62@gmail.com>
2025-04-04 18:35:10 +0800

    changes

--- Foodle Point Management System/customer_sign_up.Designer.cs
+++ Foodle Point Management System/customer_sign_up.Designer.cs
@@ -34,7 +34,7 @@
             this.lblGender = new System.Windows.Forms.Label();
             this.txtName = new System.Windows.Forms.TextBox();
             this.lblName = new System.Windows.Forms.Label();
-            this.btnReturnMain = new System.Windows.Forms.Button();
+            this.btnMain = new System.Windows.Forms.Button();
             this.lblTitle = new System.Windows.Forms.Label();
             this.txtPhoneNum = new System.Windows.Forms.TextBox();
             this.lblPhoneNum = new System.Windows.Forms.Label();
@@ -44,20 +44,18 @@
             // txtEmail
             // 
             this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtEmail.Location = new System.Drawing.Point(456, 453);
-            this.txtEmail.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtEmail.Location = new System.Drawing.Point(304, 290);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(319, 44);
+            this.txtEmail.Size = new System.Drawing.Size(214, 30);
             this.txtEmail.TabIndex = 44;
             // 
             // lblEmail
             // 
             this.lblEmail.AutoSize = true;
             this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEmail.Location = new System.Drawing.Point(310, 453);
-            this.lblEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEmail.Location = new System.Drawing.Point(207, 290);
             this.lblEmail.Name = "lblEmail";
-            this.lblEmail.Size = new System.Drawing.Size(129, 42);
+            this.lblEmail.Size = new System.Drawing.Size(86, 29);
             this.lblEmail.TabIndex = 43;
             this.lblEmail.Text = "Email：";
             // 
@@ -69,95 +67,84 @@
             this.cmbGender.Items.AddRange(new object[] {
             "Male",
             "Female"});
-            this.cmbGender.Location = new System.Drawing.Point(456, 372);
-            this.cmbGender.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbGender.Location = new System.Drawing.Point(304, 238);
             this.cmbGender.Name = "cmbGender";
-            this.cmbGender.Size = new System.Drawing.Size(319, 45);
+            this.cmbGender.Size = new System.Drawing.Size(214, 33);
             this.cmbGender.TabIndex = 42;
             // 
             // lblGender
             // 
             this.lblGender.AutoSize = true;
             this.lblGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblGender.Location = new System.Drawing.Point(290, 372);
-            this.lblGender.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblGender.Location = new System.Drawing.Point(193, 238);
             this.lblGender.Name = "lblGender";
-            this.lblGender.Size = new System.Drawing.Size(153, 42);
+            this.lblGender.Size = new System.Drawing.Size(100, 29);
             this.lblGender.TabIndex = 36;
             this.lblGender.Text = "Gender:";
             // 
             // txtName
             // 
             this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtName.Location = new System.Drawing.Point(456, 295);
-            this.txtName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtName.Location = new System.Drawing.Point(304, 189);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(319, 44);
+            this.txtName.Size = new System.Drawing.Size(214, 30);
             this.txtName.TabIndex = 34;
-            //this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
-
             // 
             // lblName
             // 
             this.lblName.AutoSize = true;
             this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblName.Location = new System.Drawing.Point(244, 295);
-            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblName.Location = new System.Drawing.Point(163, 189);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(197, 42);
+            this.lblName.Size = new System.Drawing.Size(130, 29);
             this.lblName.TabIndex = 33;
             this.lblName.Text = "Full Name:";
             // 
-            // btnReturnMain
+            // btnMain
             // 
-            this.btnReturnMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnReturnMain.Location = new System.Drawing.Point(48, 41);
-            this.btnReturnMain.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.btnReturnMain.Name = "btnReturnMain";
-            this.btnReturnMain.Size = new System.Drawing.Size(324, 70);
-            this.btnReturnMain.TabIndex = 32;
-            this.btnReturnMain.Text = "Return to Main Page";
-            this.btnReturnMain.UseVisualStyleBackColor = true;
-            this.btnReturnMain.Click += new System.EventHandler(this.btnReturnMain_Click);
+            this.btnMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMain.Location = new System.Drawing.Point(32, 26);
+            this.btnMain.Name = "btnMain";
+            this.btnMain.Size = new System.Drawing.Size(135, 45);
+            this.btnMain.TabIndex = 32;
+            this.btnMain.Text = "Main Page";
+            this.btnMain.UseVisualStyleBackColor = true;
+            this.btnMain.Click += new System.EventHandler(this.btnMain_Click);
             // 
             // lblTitle
             // 
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.Location = new System.Drawing.Point(256, 178);
-            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitle.Location = new System.Drawing.Point(171, 114);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(724, 42);
+            this.lblTitle.Size = new System.Drawing.Size(484, 29);
             this.lblTitle.TabIndex = 31;
             this.lblTitle.Text = "Please Fill In Your Customer Information";
             // 
             // txtPhoneNum
             // 
             this.txtPhoneNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPhoneNum.Location = new System.Drawing.Point(456, 534);
-            this.txtPhoneNum.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPhoneNum.Location = new System.Drawing.Point(304, 342);
             this.txtPhoneNum.Name = "txtPhoneNum";
-            this.txtPhoneNum.Size = new System.Drawing.Size(319, 44);
+            this.txtPhoneNum.Size = new System.Drawing.Size(214, 30);
             this.txtPhoneNum.TabIndex = 46;
             // 
             // lblPhoneNum
             // 
             this.lblPhoneNum.AutoSize = true;
             this.lblPhoneNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPhoneNum.Location = new System.Drawing.Point(158, 534);
-            this.lblPhoneNum.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPhoneNum.Location = new System.Drawing.Point(105, 342);
             this.lblPhoneNum.Name = "lblPhoneNum";
-            this.lblPhoneNum.Size = new System.Drawing.Size(288, 42);
+            this.lblPhoneNum.Size = new System.Drawing.Size(188, 29);
             this.lblPhoneNum.TabIndex = 45;
             this.lblPhoneNum.Text = "Phone Number：";
             // 
             // btnSignUp
             // 
             this.btnSignUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSignUp.Location = new System.Drawing.Point(903, 552);
-            this.btnSignUp.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSignUp.Location = new System.Drawing.Point(602, 353);
             this.btnSignUp.Name = "btnSignUp";
-            this.btnSignUp.Size = new System.Drawing.Size(231, 67);
+            this.btnSignUp.Size = new System.Drawing.Size(154, 43);
             this.btnSignUp.TabIndex = 47;
             this.btnSignUp.Text = "Sign Up";
             this.btnSignUp.UseVisualStyleBackColor = true;
@@ -165,9 +152,9 @@
             // 
             // frmCustomerSignUp
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 703);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btnSignUp);
             this.Controls.Add(this.txtPhoneNum);
             this.Controls.Add(this.lblPhoneNum);
@@ -177,9 +164,8 @@
             this.Controls.Add(this.lblGender);
             this.Controls.Add(this.txtName);
             this.Controls.Add(this.lblName);
-            this.Controls.Add(this.btnReturnMain);
+            this.Controls.Add(this.btnMain);
             this.Controls.Add(this.lblTitle);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmCustomerSignUp";
             this.Text = "Sign Up as Customer";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.customer_sign_up_FormClosing);
@@ -195,7 +181,7 @@
         private System.Windows.Forms.Label lblGender;
         private System.Windows.Forms.TextBox txtName;
         private System.Windows.Forms.Label lblName;
-        private System.Windows.Forms.Button btnReturnMain;
+        private System.Windows.Forms.Button btnMain;
         private System.Windows.Forms.Label lblTitle;
         private System.Windows.Forms.TextBox txtPhoneNum;
         private System.Windows.Forms.Label lblPhoneNum;
