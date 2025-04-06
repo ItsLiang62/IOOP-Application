e67505ad6b8a9038adaf100dd24ad1f9ccf97a14
ItsLiang62 <andersonwlx62@gmail.com>
2025-04-06 20:44:02 +0800

    Update input_checker.cs

--- Shared Class Library/input_checker.cs
+++ Shared Class Library/input_checker.cs
@@ -91,7 +91,7 @@ namespace Shared_Class_Library
         {
             if (!DateTime.TryParseExact(input, "d/M/yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime tempDate))
             {
-                errorMessage = "Invalid date. Make sure the date has no leading zeroes";
+                errorMessage = "Invalid date. Make sure the date follows format d/M/yyyy. (E.g. 9/9/2005)";
                 return false;
             }
 
